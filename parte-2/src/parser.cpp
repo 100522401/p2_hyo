7952359d94ef1ff06cfa0a80e8845e13f77a6009
egonzalez100522276 <100522276@alumnos.uc3m.es>
2025-11-22 18:17:54 +0100

    add parseEdges. Graph parser complete.

--- parte-2/src/parser.cpp
+++ parte-2/src/parser.cpp
@@ -5,7 +5,7 @@
 #include <sstream>
 #include <string>
 
-void parseCoords(const std::string &infile, Graph &g) {
+void parseNodes(const std::string &infile, Graph &g) {
   // Open file
   std::ifstream in(infile);
   if (!in.is_open()) {
@@ -47,20 +47,66 @@ void parseCoords(const std::string &infile, Graph &g) {
             << std::endl;
 }
 
+void parseEdges(std::string const &infile, Graph &g) {
+  // Open file
+  std::ifstream in(infile);
+  if (!in.is_open()) {
+    std::cerr << "Error al abrir el archivo: " << infile << std::endl;
+    return;
+  }
+
+  // Read file line by line
+  std::string line;
+  while (std::getline(in, line)) {
+    // Ignore empty and commented lines
+    if (line.empty() || line[0] == '#')
+      continue;
+
+    // Definitions
+    std::istringstream iss(line);
+    std::string prefix;
+    int id;
+    int u, v; // Nodes
+    int distance;
+
+    // Structure of the line
+    iss >> prefix >> u >> v >> distance;
+
+    // Only process lines that start with 'a'
+    if (prefix != "a")
+      continue;
+
+    // Add edge
+    g.addEdge(u, v, distance);
+  }
+
+  in.close();
+
+  std::cout << "Se han leído " << g.nodes.size() << " nodos desde " << infile
+            << std::endl;
+}
+
 int main() {
   std::string co_file = "../graph-data/USA-road-d.USA.co";
+  std::string gr_file = "../graph-data/USA-road-d.USA.gr";
 
   Graph g;
 
   std::cout << "Leyendo nodos desde " << co_file << " ..." << std::endl;
-
   auto start = std::chrono::high_resolution_clock::now();
-  parseCoords(co_file, g);
+  parseNodes(co_file, g);
   auto end = std::chrono::high_resolution_clock::now();
-
   std::chrono::duration<double> elapsed = end - start;
-  std::cout << "Tiempo de parseo: " << elapsed.count() << " segundos."
-            << std::endl;
+  std::cout << "Tiempo de parseo de nodos: " << elapsed.count()
+            << " segundos.\n";
+
+  std::cout << "Leyendo aristas desde " << gr_file << " ..." << std::endl;
+  start = std::chrono::high_resolution_clock::now();
+  parseEdges(gr_file, g);
+  end = std::chrono::high_resolution_clock::now();
+  elapsed = end - start;
+  std::cout << "Tiempo de parseo de aristas: " << elapsed.count()
+            << " segundos.\n";
 
   std::cout << "Grafo cargado. Número de nodos: " << g.nodes.size()
             << std::endl;
@@ -68,10 +114,13 @@ int main() {
   std::cout << std::fixed << std::setprecision(6);
   for (size_t i = 0; i < g.nodes.size() && i < 10; ++i) {
     const GNode &node = g.getNode(i + 1);
-
     std::cout << "Nodo " << node.id << " con coordenadas (" << node.cx << ", "
-              << node.cy << ")"
-              << " y " << node.neighbours.size() << " vecinos" << std::endl;
+              << node.cy << ") y " << node.neighbours.size() << " vecinos:\n";
+
+    for (const auto &edge : node.neighbours) {
+      std::cout << "    -> Nodo " << edge.to + 1 << " a distancia "
+                << edge.distance << "\n";
+    }
   }
 
   return 0;
