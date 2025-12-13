f7d4f3f6d8613ba2b8f1d30495c7f288be9e3c1e
egonzalez100522276 <100522276@alumnos.uc3m.es>
2025-12-13 13:31:59 +0100

    add haversine distace as heuristic function

--- parte-2/src/algorithm.cpp
+++ parte-2/src/algorithm.cpp
@@ -1,1 +1,26 @@
 #include "algorithm.hpp"
+#include <cmath>
+
+double Algorithm::h(int n) {
+  // Haversine distance in meters
+  double lat1 = g_.coords[n].lat;
+  double lon1 = g_.coords[n].lon;
+  double lat2 = g_.coords[goal_].lat;
+  double lon2 = g_.coords[goal_].lon;
+
+  // Convert degrees to radians
+  lat1 *= M_PI / 180.0;
+  lon1 *= M_PI / 180.0;
+  lat2 *= M_PI / 180.0;
+  lon2 *= M_PI / 180.0;
+
+  double dlat = lat2 - lat1;
+  double dlon = lon2 - lon1;
+
+  double a = sin(dlat / 2) * sin(dlat / 2) +
+             cos(lat1) * cos(lat2) * sin(dlon / 2) * sin(dlon / 2);
+  double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+
+  const double R = 6371000; // Earth radius in meters
+  return R * c;
+}
