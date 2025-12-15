dacd4910b0d65128e39eb4bd641aea8eb964fce5
egonzalez100522276 <100522276@alumnos.uc3m.es>
2025-12-15 17:52:26 +0100

    add proper route handling for parte-2.py

--- parte-2/parte-2.py
+++ parte-2/parte-2.py
@@ -4,27 +4,42 @@ import sys
 import subprocess
 from pathlib import Path
 
+
 def main():
     if len(sys.argv) != 5:
-        print("Uso: ./programa <vertice-1> <vertice-2> <nombre-del-mapa> <fichero-salida>")
+        print(
+            "Uso: ./programa <vertice-1> <vertice-2> <nombre-del-mapa> <fichero-salida>"
+        )
         return 1
 
     u = sys.argv[1]
     v = sys.argv[2]
     map_name = sys.argv[3]
+    # Resolver ruta de los ficheros de mapa.
+    map_path = Path(map_name).resolve()
+    co_file = Path(map_name + ".co").resolve()
+    gr_file = Path(map_name + ".gr").resolve()
+    map_paths = [co_file, gr_file]
+
+    # Comprobación existencia mapa
+    if not all(map_path.exists() for map_path in map_paths):
+        print(f"Error: no existe el mapa: {map_name}", file=sys.stderr)
+        return 1
+
     output_file = sys.argv[4]
 
     # Comprobación existencia binario
     root = Path(__file__).resolve().parent
     exe = root / "build" / "graph_parser"
-    
+
     if not exe.exists():
         print(f"Error: no existe el binario: {exe}", file=sys.stderr)
         return 1
-    
+
     # ejecutar: ./parte-2 u v map_name out_file
-    cmd = [str(exe), u, v, map_name, output_file]
+    cmd = [str(exe), u, v, map_path, output_file]
     return subprocess.call(cmd)
+
 
 if __name__ == "__main__":
     raise SystemExit(main())
